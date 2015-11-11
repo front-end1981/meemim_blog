7be2e52866f31531fc3e7ce1fe92611900df8591
Alexander Dyachenko <alexda81@gmail.com>
2015-11-12 00:54:09 +0200

    markup page blog index

--- wp-content/themes/twentyfourteen/single.php
+++ wp-content/themes/twentyfourteen/single.php
@@ -11,6 +11,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
+
 			<?php
 				// Start the Loop.
 				while ( have_posts() ) : the_post();
